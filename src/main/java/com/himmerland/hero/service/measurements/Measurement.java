098862849c496bf96fff3bb37f94e35287009102
MikkelVintherJensen <mvje08@student.aau.dk>
2025-11-04 12:59:40 +0100

    backend fully functional but with no refactoring of code

---
+++ src/main/java/com/himmerland/hero/service/measurements/Measurement.java
@@ -0,0 +1,37 @@
+package com.himmerland.hero.service.measurements;
+
+public class Measurement {
+    private String meterNumber;
+    private String meterType;
+    private String consumptionType;
+    private String timestamp;
+    private int infoCode = 0;
+
+    public Measurement(String meterNumber, String meterType, String consumptionType, String timestamp, int infoCode) {
+        this.meterNumber = meterNumber;
+        this.meterType = meterType;
+        this.consumptionType = consumptionType;
+        this.timestamp = timestamp;
+        this.infoCode = infoCode;
+    }
+
+    public String getMeterNumber() {
+        return meterNumber;
+    }
+
+    public String getMeterType() {
+        return meterType;
+    }
+
+    public String getConsumptionType() {
+        return consumptionType;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public int getInfoCode() {
+        return infoCode;
+    }
+}
