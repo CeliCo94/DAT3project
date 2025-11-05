96e48576883a45f855ea3ab4913d1bec5cf173ff
MikkelVintherJensen <mvje08@student.aau.dk>
2025-11-05 09:45:40 +0100

    display of notification on webpage

--- src/main/java/com/himmerland/hero/service/notifications/Notification.java
+++ src/main/java/com/himmerland/hero/service/notifications/Notification.java
@@ -13,11 +13,14 @@ public class Notification {
     private boolean isActive;
     private boolean isSent;
 
-    public Notification(String address, String cause, String rule, int criticalityLevel, String timeStamp, boolean isActive, boolean isSent) {
+    public Notification() {
+    }
+
+    public Notification(String address, String cause, String rule, Criticality criticality, String timeStamp, boolean isActive, boolean isSent) {
         this.address = address;
         this.cause = cause;
         this.rule = rule;
-        this.criticality = Criticality.values()[criticalityLevel];
+        this.criticality = criticality;
         this.timeStamp = timeStamp;
         this.isActive = true;
         this.isSent = false;
@@ -26,22 +29,46 @@ public class Notification {
     public String getAddress() {
         return address;
     }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public String getCause() {
         return cause;
     }
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
     public String getRule() {
         return rule;
+    }
+    public void setRule(String rule) {
+        this.rule = rule;
     }
     public Criticality getCriticality() {
         return criticality;
     }
+    public void setCriticality(Criticality criticality) {
+        this.criticality = criticality;
+    }
+
     public String getTimeStamp() {
         return timeStamp;
+    }
+    public void setTimeStamp(String timeStamp) {
+        this.timeStamp = timeStamp;
     }
     public boolean isActive() {
         return isActive;
     }
+    public void setActive(boolean active) {
+        isActive = active;
+    }
     public boolean isSent() {
         return isSent;
+    }
+    public void setSent(boolean sent) {
+        isSent = sent;
     }
 }
