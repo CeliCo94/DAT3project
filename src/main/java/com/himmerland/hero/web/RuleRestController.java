feafc2e55d01e24b9689081fc8b26295ffe012a3
Mulvarperercool <xpouls24@student.aau.dk>
2025-11-06 11:01:08 +0100

    HTML side for regler samt get request for data input i formularing (hvilket virker... tror jeg..)

--- src/main/java/com/himmerland/hero/web/RuleRestController.java
+++ src/main/java/com/himmerland/hero/web/RuleRestController.java
@@ -1,8 +1,26 @@
 package com.himmerland.hero.web;
 
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.himmerland.hero.service.rules.RuleThresholdHeat;
+
+// Render HTML content 
 @RestController
 public class RuleRestController {
+    // Maybe her mit problem er?
+    @GetMapping("/Regler") 
+    public String ruleForm(Model model) {
+        model.addAttribute("rule", new RuleThresholdHeat());
+        return "rule";
+    }
 
+    @PostMapping("/Regler") 
+    public String ruleSubmit(@ModelAttribute RuleThresholdHeat rule, Model model) {
+        model.addAttribute("rule", rule);
+        return "result";
+    }
 }
