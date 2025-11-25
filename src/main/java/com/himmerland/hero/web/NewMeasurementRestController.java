b174622460c54b1a86dff2acfa505a555f4b98fd
MikkelVintherJensen <mvje08@student.aau.dk>
2025-11-25 09:27:37 +0100

    structure working - implementation missing

--- src/main/java/com/himmerland/hero/web/NewMeasurementRestController.java
+++ src/main/java/com/himmerland/hero/web/NewMeasurementRestController.java
@@ -3,6 +3,7 @@ package com.himmerland.hero.web;
 import com.himmerland.hero.domain.measurements.MeasurementHeat;
 import com.himmerland.hero.service.monitoring.MonitoringService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class NewMeasurementRestController {
     }
 
     // Temporary test endpoint
-    @PostMapping("/measurements/test")
+    @GetMapping("/measurements/test")
     public ResponseEntity<Void> onNewMeasurement() {
         MeasurementHeat measurement = new MeasurementHeat();
         measurement.setVolume(700); // just a dummy value for now
