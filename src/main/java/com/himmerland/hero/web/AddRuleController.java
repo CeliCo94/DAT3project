6b97142f000c2824bf413621ba2fd93892154bef
MikkelVintherJensen <mvje08@student.aau.dk>
2025-12-02 10:18:02 +0100

    Front-end working an add rule

---
+++ src/main/java/com/himmerland/hero/web/AddRuleController.java
@@ -0,0 +1,24 @@
+package com.himmerland.hero.web;
+
+import com.himmerland.hero.domain.rules.RuleThresholdHeat;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+@RequestMapping("/api/tilføj-regel")
+@CrossOrigin
+public class AddRuleController {
+
+  @PostMapping(consumes = "application/json", produces = "application/json")
+  public ResponseEntity<RuleThresholdHeat> submitRule(@RequestBody RuleThresholdHeat rule) {
+    System.out.println("Received rule: " + rule);
+    //Application.evaluateRuleThresholdHeat(rule);
+    return ResponseEntity.status(201).body(rule);
+  }
+}
