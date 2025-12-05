bdb54ab8a39a37b6f33bc61551f5b4f7cfd8a3ce
Sljuha <115470781+Sljuha@users.noreply.github.com>
2025-12-05 12:08:04 +0100

    MeterRepository

---
+++ src/main/java/com/himmerland/hero/repositories/MeterRepository.java
@@ -0,0 +1,16 @@
+package com.himmerland.hero.repositories;
+
+import com.himmerland.hero.domain.meters.Meter;
+import java.nio.file.Path;
+
+public class MeterRepository extends BaseRepository<Meter> {
+
+    public MeterRepository(Path dataDir) {
+        super(dataDir, "meters", Meter.class);
+    }
+
+    @Override
+    protected Class<Meter> getEntityClass() {
+        return Meter.class;
+    }
+}
