0659f96b2811a5e1dd104530310b22caba0269b6
TheRedLeaf <j-sadiaas@hotmail.com>
2025-12-09 17:44:05 +0100

    Monitorservice like main

--- src/main/java/com/himmerland/hero/service/monitoring/MonitoringService.java
+++ src/main/java/com/himmerland/hero/service/monitoring/MonitoringService.java
@@ -1,23 +1,31 @@
 package com.himmerland.hero.service.monitoring;
-import com.himmerland.hero.service.ruleEngine.RuleEngine;
-import java.util.List;
-import org.springframework.stereotype.Service;
+
 import com.himmerland.hero.domain.measurements.Measurement;
+import com.himmerland.hero.domain.notifications.Notification;
+import com.himmerland.hero.service.repositories.MeterRepository;
 import com.himmerland.hero.service.ruleEngine.RuleContext;
+import com.himmerland.hero.service.ruleEngine.RuleEngine;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MonitoringService {
-    private RuleEngine ruleEngine;
 
-    public MonitoringService(RuleEngine ruleEngine) {
+    private final RuleEngine ruleEngine;
+    private final MeterRepository meterRepository;
+
+    public MonitoringService(RuleEngine ruleEngine, MeterRepository meterRepository) {
         this.ruleEngine = ruleEngine;
-        System.out.println("MonitoringService has been instantiated");
+        this.meterRepository = meterRepository;
     }
 
     public void handleNewMeasurement(Measurement measurement) {
-        RuleContext ruleContext = new RuleContext();
-        List<?> notifications = ruleEngine.onNewMeasurement(measurement, ruleContext);
-        System.out.println("Number of notification in MonitoringService: " + notifications.size());
-    }
+        RuleContext ruleContext = new RuleContext(meterRepository);
 
+        List<Notification> notifications =
+                ruleEngine.onNewMeasurement(measurement, ruleContext);
+
+        System.out.println("Notifications: " + notifications.size());
+    }
 }
